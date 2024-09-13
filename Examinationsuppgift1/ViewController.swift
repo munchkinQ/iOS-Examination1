f870eddb0db0c045a9819068733364306b6084a3
Åsa Östmark <asa.oestmark@gmail.com>
2024-09-13 12:47:50 +0200

    set up basic storyboard ui

---
+++ Examinationsuppgift1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examinationsuppgift1
+//
+//  Created by Åsa Östmark on 2024-09-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
